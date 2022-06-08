5ab577fa1cca87bacef4c69dd98ebe96a89a3bc8
Carter Ball <carter.k.ball@gmail.com>
2022-06-08 17:41:52 -0400

    Add commas where they were missing

--- comparisons.rb
+++ comparisons.rb
@@ -39,17 +39,17 @@ puts "Is number_teachers not equal to number_students?", number_teachers =! numb
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
 
-puts "Is number_students greater than or equal to 20?" number_students >= 20
+puts "Is number_students greater than or equal to 20?", number_students >= 20
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
 
-puts "Is number_studnets greater than or equal to 21?" number_students >= 21
+puts "Is number_studnets greater than or equal to 21?", number_students >= 21
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
 
-puts "Is number_students less than or equal to 20?" number_students <= 20
+puts "Is number_students less than or equal to 20?", number_students <= 20
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
