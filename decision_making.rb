dc945fbb0d636efd2d4aede2c87fa4a1646eb64a
Carter Ball <carter.k.ball@gmail.com>
2022-06-08 19:29:33 -0400

    Add answers to the questions in decision_making.rb

--- decision_making.rb
+++ decision_making.rb
@@ -35,9 +35,18 @@ end
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+    ###This is an else statement. It is determining the value of the string output from the variable bear_clothing based
+    ###on the door choice.
 # 2. What variable has a new value assigned to it after the first if statement executes?
+    ###bear_clothing
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+    ### "scarf"
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+    ### This is an else statement. It is determining what the final thing is that happens in the story/the final output based on
+    ### the integer value of the variable bear_choice.
 # 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
+    ### "You run as fast as you can into the next room. It's full of snakes!"
 # 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
+    ### "You tell the bear the hat is too small and it starts to cry!"
 # 7. What is your favorite ending?
+    ### My favorite ending is "You stay with the bear and become it's best friend!"
