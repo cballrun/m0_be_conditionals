8e4c3cb20ce5663dc7b044b712f8275c04ef7287
Carter Ball <carter.k.ball@gmail.com>
2022-06-08 21:03:30 -0400

    Add if statement regarding whether pizza can be made

--- if_statements.rb
+++ if_statements.rb
@@ -66,5 +66,12 @@ end
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
+
+
 cups_of_flour = 1
 has_sauce = true
+
+if cups_of_flour = 1 && has_sauce = true
+  puts "I cannot make pizza"
+
+end
