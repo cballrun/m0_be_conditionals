fc1d6aaa64728ac0d8f10a5cf99530fe0e7c2c62
Carter Ball <carter.k.ball@gmail.com>
2022-06-08 21:27:20 -0400

    Change both variables to integers

--- code_challenges/code_challenges.rb
+++ code_challenges/code_challenges.rb
@@ -5,8 +5,8 @@
 #- If the user either has a good record or is over 25 years old, they should pay full price
 #- If the user is not over 25 and has a bad driving record, they need to have someone else sign for the rental
 
-good_driving_record = false
-is_over_25 = true
+good_driving_record = 5
+is_over_25 = 4
 
 if good_driving_record == true && is_over_25 == true
   puts "They should get a discount"
